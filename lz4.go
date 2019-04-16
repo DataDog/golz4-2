bf281f144e0b4a0b586712ffef17a0a4f1b8f997
shuyang.zhao <shuyangzh@gmail.com>
2019-04-16 10:56:56 -0400

    added go interface for lz4 streaming API

--- lz4.go
+++ lz4.go
@@ -1,13 +1,47 @@
 package lz4
 
-// #cgo CFLAGS: -O3
-// #include "src/lz4.h"
-// #include "src/lz4.c"
+/*#cgo CFLAGS: -O3
+#include "src/lz4.h"
+#include "src/lz4.c"
+
+static int LZ4_decompress_safe_continue_and_memcpy(
+	LZ4_streamDecode_t* stream,
+	const char* src, int srcSize,
+	char* dstBuf, int dstBufCapacity,
+	char* dst)
+{
+	// decompress to ringbuffer
+	int result = LZ4_decompress_safe_continue(stream, src, dstBuf, srcSize, dstBufCapacity);
+	if (result > 0) {
+		// copy decompressed data to dst
+		memcpy(dst, dstBuf, (size_t)result);
+	}
+	return result;
+}
+*/
 import "C"
 
 import (
 	"errors"
+	"runtime"
 	"unsafe"
+)
+
+const (
+	// MaxInputSize is the max supported input size. see macro LZ4_MAX_INPUT_SIZE.
+	MaxInputSize = 0x7E000000 // 2 113 929 216 bytes
+
+	minDictionarySize = 4096
+	minMessageSize    = 256
+)
+
+// Error codes
+var (
+	ErrSrcTooLarge = errors.New("src is too large")
+	ErrCompress    = errors.New("compress: dst is not large enough")
+	ErrDecompress  = errors.New("decompress: dst is not large enough, or src data is malformed")
+	ErrNoData      = errors.New("no data")
+	ErrInternal    = errors.New("internal error")
 )
 
 // p gets a char pointer to the first byte of a []byte slice
@@ -42,6 +76,16 @@ func CompressBound(in []byte) int {
 	return len(in) + ((len(in) / 255) + 16)
 }
 
+// CompressBoundInt returns the maximum size that LZ4 compression may output
+// in a "worst case" scenario (input data not compressible).
+// see macro LZ4_COMPRESSBOUND.
+func CompressBoundInt(inputSize int) int {
+	if inputSize <= 0 || inputSize > MaxInputSize {
+		return 0
+	}
+	return inputSize + inputSize/255 + 16
+}
+
 // Compress compresses in and puts the content in out. len(out)
 // should have enough space for the compressed data (use CompressBound
 // to calculate). Returns the number of bytes in the out slice.
@@ -51,4 +95,216 @@ func Compress(out, in []byte) (outSize int, err error) {
 		err = errors.New("Insufficient space for compression")
 	}
 	return
+}
+
+// ContinueCompress is a struct for lz4 streaming API
+type ContinueCompress struct {
+	dictionarySize     int
+	maxMessageSize     int
+	lz4Stream          *C.LZ4_stream_t
+	ringBuffer         []byte
+	msgLen             int
+	processTimes       int64
+	totalSrcLen        int64
+	totalCompressedLen int64
+}
+
+// NewContinueCompress returns a new ContinueCompress object.
+// Call Release when the ContinueCompress is no longer needed.
+func NewContinueCompress(dictionarySize, maxMessageSize int) *ContinueCompress {
+	if dictionarySize < minDictionarySize {
+		dictionarySize = minDictionarySize
+	}
+	if maxMessageSize < minMessageSize {
+		maxMessageSize = minMessageSize
+	}
+	if maxMessageSize > MaxInputSize {
+		maxMessageSize = MaxInputSize
+	}
+	cc := &ContinueCompress{
+		dictionarySize: dictionarySize,
+		maxMessageSize: maxMessageSize,
+		lz4Stream:      C.LZ4_createStream(),
+		ringBuffer:     make([]byte, 0, dictionarySize+maxMessageSize),
+	}
+	runtime.SetFinalizer(cc, freeContinueCompress)
+	return cc
+}
+
+func freeContinueCompress(v interface{}) {
+	v.(*ContinueCompress).Release()
+}
+
+// Release releases all the resources occupied by ContinueCompress.
+// cc cannot be used after the release.
+func (cc *ContinueCompress) Release() {
+	if cc.lz4Stream != nil {
+		C.LZ4_freeStream(cc.lz4Stream)
+		cc.lz4Stream = nil
+	}
+}
+
+// Write writes src to cc.
+func (cc *ContinueCompress) Write(src []byte) error {
+	srcLen := len(src)
+	if srcLen == 0 {
+		return nil
+	}
+	if cc.msgLen+srcLen > cc.maxMessageSize {
+		return ErrSrcTooLarge
+	}
+	if len(cc.ringBuffer)+srcLen > cap(cc.ringBuffer) {
+		return ErrInternal
+	}
+	cc.ringBuffer = append(cc.ringBuffer, src...)
+	cc.msgLen += srcLen
+	return nil
+}
+
+// MsgLen returns the length of buffered data.
+func (cc *ContinueCompress) MsgLen() int {
+	return cc.msgLen
+}
+
+// Process compresses buffered data into `dst`.
+// len(dst) should >= CompressBoundInt(cc.MsgLen()).
+func (cc *ContinueCompress) Process(dst []byte) (int, error) {
+	if cc.msgLen == 0 {
+		return 0, ErrNoData
+	}
+	if cc.msgLen > len(cc.ringBuffer) {
+		return 0, ErrInternal
+	}
+	if len(dst) < CompressBoundInt(cc.msgLen) {
+		return 0, ErrDecompress
+	}
+
+	offset := len(cc.ringBuffer) - cc.msgLen
+	result := C.LZ4_compress_fast_continue(
+		cc.lz4Stream,
+		(*C.char)(unsafe.Pointer(&cc.ringBuffer[offset])),
+		(*C.char)(unsafe.Pointer(&dst[0])),
+		C.int(cc.msgLen),
+		C.int(len(dst)),
+		1)
+	if result <= 0 {
+		return 0, ErrInternal
+	}
+
+	// Update stats
+	cc.processTimes++
+	cc.totalSrcLen += int64(cc.msgLen)
+	cc.totalCompressedLen += int64(result)
+
+	// Add and wraparound the ringbuffer offset
+	if len(cc.ringBuffer) >= cc.dictionarySize {
+		cc.ringBuffer = cc.ringBuffer[0:0]
+	}
+	// Reset msgLen
+	cc.msgLen = 0
+	return int(result), nil
+}
+
+// Stats returns statistics data.
+func (cc *ContinueCompress) Stats() (processTimes, totalSrcLen, totalCompressedLen int64) {
+	return cc.processTimes, cc.totalSrcLen, cc.totalCompressedLen
+}
+
+// ContinueDecompress implements streaming decompression.
+type ContinueDecompress struct {
+	dictionarySize       int
+	maxMessageSize       int
+	lz4Stream            *C.LZ4_streamDecode_t
+	ringBuffer           []byte
+	offset               int
+	processTimes         int64
+	totalSrcLen          int64
+	totalDecompressedLen int64
+}
+
+// NewContinueDecompress returns a new ContinueDecompress object.
+//
+// dictionarySize and maxMessageSize must be exactly the same as NewContinueCompress.
+// see LZ4_decompress_*_continue() - Synchronized mode.
+//
+// Call Release when the ContinueDecompress is no longer needed.
+func NewContinueDecompress(dictionarySize, maxMessageSize int) *ContinueDecompress {
+	if dictionarySize < minDictionarySize {
+		dictionarySize = minDictionarySize
+	}
+	if maxMessageSize < minMessageSize {
+		maxMessageSize = minMessageSize
+	}
+	if maxMessageSize > MaxInputSize {
+		maxMessageSize = MaxInputSize
+	}
+	cd := &ContinueDecompress{
+		dictionarySize: dictionarySize,
+		maxMessageSize: maxMessageSize,
+		lz4Stream:      C.LZ4_createStreamDecode(),
+		ringBuffer:     make([]byte, dictionarySize+maxMessageSize),
+	}
+	runtime.SetFinalizer(cd, freeContinueDecompress)
+	return cd
+}
+
+func freeContinueDecompress(v interface{}) {
+	v.(*ContinueDecompress).Release()
+}
+
+// Release releases all the resources occupied by cd.
+// cd cannot be used after the release.
+func (cd *ContinueDecompress) Release() {
+	if cd.lz4Stream != nil {
+		C.LZ4_freeStreamDecode(cd.lz4Stream)
+		cd.lz4Stream = nil
+	}
+}
+
+// Process decompresses `src` into `dst`.
+// len(dst) should >= uncompressed_length_of_src_data.
+func (cd *ContinueDecompress) Process(dst, src []byte) (int, error) {
+	if len(src) == 0 {
+		return 0, ErrNoData
+	}
+	if cd.offset < 0 || cd.offset >= cd.dictionarySize {
+		return 0, ErrInternal
+	}
+
+	// Decompress to ringbuffer, then copy to dst
+	var result C.int
+	if dstLen := len(dst); dstLen > 0 {
+		if dstLen > cd.maxMessageSize {
+			dstLen = cd.maxMessageSize
+		}
+
+		result = C.LZ4_decompress_safe_continue_and_memcpy(
+			cd.lz4Stream,
+			(*C.char)(unsafe.Pointer(&src[0])),
+			C.int(len(src)),
+			(*C.char)(unsafe.Pointer(&cd.ringBuffer[cd.offset])),
+			C.int(dstLen),
+			(*C.char)(unsafe.Pointer(&dst[0])))
+	}
+	if result <= 0 {
+		return 0, ErrDecompress
+	}
+
+	// Update stats
+	cd.processTimes++
+	cd.totalSrcLen += int64(len(src))
+	cd.totalDecompressedLen += int64(result)
+
+	// Add and wraparound the ringbuffer offset
+	cd.offset += int(result)
+	if cd.offset >= cd.dictionarySize {
+		cd.offset = 0
+	}
+
+	return int(result), nil
+}
+
+// Stats returns statistics data.
+func (cd *ContinueDecompress) Stats() (processTimes, totalSrcLen, totalDecompressedLen int64) {
+	return cd.processTimes, cd.totalSrcLen, cd.totalDecompressedLen
 }
--- lz4_test.go
+++ lz4_test.go
@@ -6,13 +6,18 @@
 package lz4
 
 import (
+	"bytes"
+	"fmt"
 	"io/ioutil"
+	"runtime"
 	"strings"
 	"testing"
 	"testing/quick"
 )
 
 const corpusSize = 4746
+
+var plaintext0 = []byte("aldkjflakdjf.,asdfjlkjlakjdskkkkkkkkkkkkkk")
 
 func TestCompressionRatio(t *testing.T) {
 	input, err := ioutil.ReadFile("sample.txt")
@@ -191,4 +196,154 @@ func TestFuzz(t *testing.T) {
 	if err := quick.Check(f, conf); err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestContinueCompress(t *testing.T) {
+	payload := plaintext0
+	repeat := 10000
+
+	cc := NewContinueCompress(1024, 1024)
+	cd := NewContinueDecompress(1024, 1024)
+
+	var compressed []byte
+	var allCompressed [][]byte
+	var n int
+	var err error
+
+	for i := 0; i < repeat; i++ {
+		err = cc.Write(payload)
+		if err != nil {
+			t.Errorf("Write failed: %v", err)
+		}
+		compressed = make([]byte, CompressBound(payload))
+		n, err = cc.Process(compressed)
+		if err != nil {
+			t.Errorf("Process failed: %v", err)
+		}
+		compressed = compressed[:n]
+		allCompressed = append(allCompressed, compressed)
+	}
+
+	processTimes, totalSrcLen, totalCompressedLen := cc.Stats()
+	if processTimes != int64(repeat) {
+		t.Errorf("Expect %v, got %v", repeat, processTimes)
+	}
+	ratio := float64(totalCompressedLen) / float64(totalSrcLen) * 100.0
+	t.Logf("totalSrcLen=%v, totalCompressedLen=%v, ratio=%.1f%%", totalSrcLen, totalCompressedLen, ratio)
+
+	decompressBuf := make([]byte, 4096)
+	for _, compressed = range allCompressed {
+		n, err = cd.Process(decompressBuf, compressed)
+		if err != nil {
+			t.Errorf("Process failed: %v", err)
+		}
+		if n != len(payload) {
+			fmt.Println("n:", n, "payload:", len(payload))
+			t.Errorf("Process failed: %v", err)
+		}
+
+		if string(decompressBuf[:n]) != string(payload) {
+			t.Fatalf("Did not read the same %s != %s", string(decompressBuf), string(payload))
+		}
+
+	}
+	processTimes, totalSrcLen, totalDecompressedLen := cd.Stats()
+	if processTimes != int64(repeat) {
+		t.Errorf("Expect %v, got %v", repeat, processTimes)
+	}
+	ratio = float64(totalSrcLen) / float64(totalDecompressedLen) * 100.0
+	t.Logf("totalSrcLen=%v, totalDecompressedLen=%v, ratio=%.1f%%", totalSrcLen, totalDecompressedLen, ratio)
+
+	// Let finalizer run...
+	cc = nil
+	cd = nil
+	runtime.GC()
+}
+
+func BenchmarkCompress(b *testing.B) {
+	dst := make([]byte, CompressBound(plaintext0))
+	b.SetBytes(int64(len(plaintext0)))
+	for i := 0; i < b.N; i++ {
+		_, err := Compress(dst, plaintext0)
+		if err != nil {
+			b.Errorf("Compress error: %v", err)
+		}
+	}
+}
+func BenchmarkContinueCompress(b *testing.B) {
+	cc := NewContinueCompress(32*1024, 4096)
+	defer cc.Release()
+
+	dst := make([]byte, CompressBound(plaintext0))
+	b.SetBytes(int64(len(plaintext0)))
+	for i := 0; i < b.N; i++ {
+		err := cc.Write(plaintext0)
+		if err != nil {
+			b.Errorf("Write error: %v", err)
+		}
+		_, err = cc.Process(dst)
+		if err != nil {
+			b.Errorf("Process error: %v", err)
+		}
+
+	}
+}
+
+func BenchmarkCompressUncompress(b *testing.B) {
+	compressed := make([]byte, CompressBound(plaintext0))
+	n, err := Compress(compressed, plaintext0)
+	if err != nil {
+		b.Errorf("Compress error: %v", err)
+	}
+	compressed = compressed[:n]
+
+	dst := make([]byte, len(plaintext0))
+	b.SetBytes(int64(len(plaintext0)))
+	for i := 0; i < b.N; i++ {
+		_, err := Uncompress(dst, compressed)
+		if err != nil {
+			b.Errorf("Uncompress error: %v", err)
+		}
+	}
+}
+
+func BenchmarkContinueCompressUncompress(b *testing.B) {
+	b.ReportAllocs()
+	cc := NewContinueCompress(32*1024, 4096)
+	defer cc.Release()
+
+	cd := NewContinueDecompress(32*1024, 4096)
+	defer cd.Release()
+
+	var err error
+	var n int
+
+	compressed := make([]byte, CompressBound(plaintext0))
+	decompressed := make([]byte, 4096)
+
+	b.SetBytes(int64(len(plaintext0)))
+	for i := 0; i < b.N; i++ {
+
+		err = cc.Write(plaintext0)
+		if err != nil {
+			b.Errorf("Write error: %v", err)
+		}
+		n, err = cc.Process(compressed)
+		if err != nil {
+			b.Errorf("Process error: %v", err)
+		}
+
+		n, err = cd.Process(decompressed, compressed[:n])
+		if err != nil {
+			b.Errorf("Process error: %v", err)
+		}
+
+		if i%100 == 0 {
+
+			if !bytes.Equal(plaintext0, decompressed[:n]) {
+				b.Error("decompressed != plaintext0")
+			}
+		}
+	}
+
 }
